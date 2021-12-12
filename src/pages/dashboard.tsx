8192c9314b14e7817b5a16ea4b06482cdaad2fdd
Luiz Rodrigues Junior <junior.luiz182@gmail.com>
2021-12-12 16:18:26 -0300

    Paginas de controle de usuario

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,5 @@
 import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
-import { ApexOptions } from 'apexcharts';
+
 import dynamic from "next/dynamic";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
